578e803a9e69b14b37f86a416e1c4495d232d4f5
coe <coe.app@gmail.com>
2015-05-02 12:37:30 +0900

    実体ファイルをコミット

---
+++ iphone/LLInstalledApps.h
@@ -0,0 +1,19 @@
+//
+//  LLInstalledApps.h
+//  iOSInstalledApps
+//
+//  Created by Aravind Krishnaswamy on 02/09/12.
+//  Copyright (c) 2012 Aravind Krishnaswamy. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LLInstalledApps : NSObject
+
++ (NSArray *) runningApps;
+- (BOOL) appsInstalledWithScheme:(NSString *)applicationScheme;
++ (NSDictionary *) appsInstalledWithSchemes:(NSArray *)applicationSchemes;
++ (NSDictionary* ) appsInstalledWithSchemes:(NSArray *)applicationSchemes withProgressCallback:(void (^)(id))callbackBlock;
++ (NSArray* ) appsInstalledWithSchemes:(NSArray *)applicationSchemes withProgressCallback:(void (^)(id))callbackBlock maxSize:(int) maxSize;
+
+@end
